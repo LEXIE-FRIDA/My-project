fc06fe07030f173b0b053520933a6479524c9ed5
LEXIE-FRIDA <lexie.frida@student.moringaschool.com>
2024-11-22 12:19:14 +0300

    initial commit

---
+++ pets.py
@@ -0,0 +1,24 @@
+print("Welcome to our pet store.Take a look at our pets")
+list=[
+{"name":"Max","color":"ginger","animal":"dog"},
+{"name":"Sky","color":"white","animal":"parot"},
+{"name":"Marshall","color":"black","animal":"cat"},
+{"name":"Ben","color":"grey","animal":"sheep"}
+]
+
+choices =''
+print("[1] Press 1 to take a look at Max \n")
+print("[2] Press 2 to take a look at Sky \n")
+print("[3] Press 3 to take a look at Marshall \n")
+print("[4] Press 4 to take a look at Ben \n")
+
+if choices == '1':
+    print("Woof Woof \n")
+
+elif choices == '2':
+    print("Hiii \n")
+elif choices == '3':
+    print("Meoww \n")
+elif choices == '4':
+    print("Baaaaa \n")
+
